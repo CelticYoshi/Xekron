3cf58de9136372f8d8e23fe38bd005ffc574d8bc
Drake B <drakeboard719@riversideunified.org>
2025-01-24 09:22:51 -0800

    Added enemy movement

---
+++ Xekron/Assets/Drake's Scripts/EnemyMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 1f;
+    public float rangeValue = 5f;
+    private Vector3 _startingPosition;
+    [SerializeField] private bool _isAttacking;
+    [SerializeField] private Transform _player;
+    [SerializeField] private Animator _enemyAnimation;
+    [SerializeField] private Rigidbody _enemyRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movementDirection = (_player.position - transform.position).normalized;
+        
+        float distance = Vector3.Distance(_player.position, transform.position);
+        //Debug.Log("Distance" + distance);
+        if(distance < rangeValue)
+            {
+                _enemyRb.velocity = movementDirection * speed;
+                transform.LookAt(_player);
+            }
+            
+}}
