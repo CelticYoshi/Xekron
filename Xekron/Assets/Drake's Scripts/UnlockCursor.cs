9164df9297a62e400d29ad551939ba3be4962855
Drake B <drakeboard719@riversideunified.org>
2025-05-07 09:15:35 -0700

    Tried to unlock cursor

---
+++ Xekron/Assets/Drake's Scripts/UnlockCursor.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockCursor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Cursor.lockState = CursorLockMode.Unlocked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
