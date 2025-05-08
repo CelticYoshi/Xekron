db0d0ed5e42e8bfc53dd8f849dd139a9e3d6a124
Drake B <drakeboard719@riversideunified.org>
2025-05-08 09:08:36 -0700

    added quit button

--- Xekron/Assets/Drake's Scripts/UnlockCursor.cs
+++ Xekron/Assets/Drake's Scripts/UnlockCursor.cs
@@ -7,7 +7,7 @@ public class UnlockCursor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Cursor.lockState = CursorLockMode.Unlocked;
+        Cursor.lockState = CursorLockMode.None;
     }
 
     // Update is called once per frame
